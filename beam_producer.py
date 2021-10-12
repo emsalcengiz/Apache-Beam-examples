27ad6b0787928655ceafb942996e7ed34a466b5d
Emsal CENGİZ <30407530+emsalcengiz@users.noreply.github.com>
2021-10-12 21:58:53 +0300

    Initial commit

---
+++ beam_producer.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from beam_nuggets.io import kafkaio
+
+with beam.Pipeline(options=PipelineOptions()) as p:
+    events = (p
+                     | "Creating data" >> beam.Create([('dev_1', '{"device": "0003", status": "healthy"}')])
+                     | "Pushing messages to Kafka" >> kafkaio.KafkaProduce(
+                                                                            topic='notifications',
+                                                                            servers="*.*.*.*:9092"
+                                                                        )
+                    )
+    events | 'Writing to stdout' >> beam.Map(print)
